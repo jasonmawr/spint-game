2578b5c06c4cac8d0aaed6e2e790fdb43c84c997
jasonmawr <105283127+jasonmawr@users.noreply.github.com>
2023-03-23 16:54:49 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,75 @@
+// Immediately invoked function expression
+// to not pollute the global scope
+(function() {
+  const wheel = document.querySelector('.wheel');
+  const startButton = document.querySelector('.button');
+  const display = document.querySelector('.display');
+  
+  let deg = 0;
+  let zoneSize = 45; // deg
+
+var symbolSegments = [];
+
+fetch('https://641b376c9b82ded29d4d2cfa.mockapi.io/spint-game', {
+  method: 'GET',
+  headers: { 'Content-Type': 'application/json' },
+}).then(res => res.json()).then(data => {
+  let result = data;
+  console.log(result);
+  result.map(i =>
+    symbolSegments.push(i.gift_name)
+  );
+  console.log(symbolSegments);
+});
+
+
+
+  // Counter clockwise
+  // const symbolSegments = {
+  //   1: "Frog",
+  //   2: "Snail",
+  //   3: "Dolphin",
+  //   4: "Ladybug",
+  //   5: "Koala",
+  //   6: "Unicorn",
+  //   7: "Dragon",
+  //   8: "Snowman",
+  // }
+
+  const handleWin = (actualDeg) => {
+    const winningSymbolNr = Math.ceil(actualDeg / zoneSize);
+    display.innerHTML = symbolSegments[winningSymbolNr];
+  }
+
+  startButton.addEventListener('click', () => {
+    // Reset display
+    display.innerHTML = "-";
+    // Disable button during spin
+    startButton.style.pointerEvents = 'none';
+    // Calculate a new rotation between 5000 and 10 000
+    deg = Math.floor(5000 + Math.random() * 5000);
+    // Set the transition on the wheel
+    wheel.style.transition = 'all 4s ease-out';
+    // Rotate the wheel
+    wheel.style.transform = `rotate(${deg}deg)`;
+    // Apply the blur
+    wheel.classList.add('blur');
+  });
+
+  wheel.addEventListener('transitionend', () => {
+    // Remove blur
+    wheel.classList.remove('blur');
+    // Enable button when spin is over
+    startButton.style.pointerEvents = 'auto';
+    // Need to set transition to none as we want to rotate instantly
+    wheel.style.transition = 'none';
+    // Calculate degree on a 360 degree basis to get the "natural" real rotation
+    // Important because we want to start the next spin from that one
+    // Use modulus to get the rest value
+    const actualDeg = deg % 360;
+    // Set the real rotation instantly without animation
+    wheel.style.transform = `rotate(${actualDeg}deg)`;
+    // Calculate and display the winning symbol
+    handleWin(actualDeg);
+  });
+})();
